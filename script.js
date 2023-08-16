14fa19ee06c8ed93c10c9689fff29b86666e3981
guidobtg97 <guidobtg97@gmail.com>
2023-08-16 18:06:47 -0300

    pre version final

--- script.js
+++ script.js
@@ -20,3 +20,55 @@ navItems.forEach((item) => {
     targetElement.scrollIntoView({ behavior: 'smooth' });
   });
 });
+
+
+// Obtener elementos del DOM
+const sliderContainer = document.querySelector('.slider-container');
+const prevButton = document.querySelector('.prev-btn');
+const nextButton = document.querySelector('.next-btn');
+
+// Configurar índice de imagen actual
+let currentIndex = 0;
+
+// Función para mostrar la imagen actual
+function showImage(index) {
+    const images = sliderContainer.querySelectorAll('img');
+    images.forEach((image, i) => {
+        if (i === index) {
+            image.style.display = 'block';
+        } else {
+            image.style.display = 'none';
+        }
+    });
+}
+
+// Función para avanzar a la siguiente imagen
+function nextImage() {
+    currentIndex = (currentIndex + 1) % 5;
+    showImage(currentIndex);
+}
+
+// Función para retroceder a la imagen anterior
+function prevImage() {
+    currentIndex = (currentIndex - 1 + 5) % 5;
+    showImage(currentIndex);
+}
+
+// Agregar eventos a los botones de flecha
+prevButton.addEventListener('click', prevImage);
+nextButton.addEventListener('click', nextImage);
+
+// Mostrar la primera imagen al cargar la página
+showImage(currentIndex);
+
+
+window.addEventListener('scroll', function () {
+  var whatsappButton = document.querySelector('.whatsapp-button');
+  var scrollPosition = window.scrollY || window.pageYOffset;
+
+  if (scrollPosition > 100) {
+    whatsappButton.style.bottom = '70px';
+  } else {
+    whatsappButton.style.bottom = '20px';
+  }
+});
